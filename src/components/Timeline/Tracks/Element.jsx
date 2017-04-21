5ba57af86dae8b63aeb38791762d4782c67ce988
Robert Smith <rbrtsmith84@googlemail.com>
2017-04-21 15:44:38 +0100

    fix padding bug on track-elements with very small rendered width

--- src/components/Timeline/Tracks/Element.jsx
+++ src/components/Timeline/Tracks/Element.jsx
@@ -2,17 +2,22 @@ import React, { PropTypes } from 'react'
 import BasicElement from '../../Elements/Basic'
 import datePropType from '../../../utils/datePropType'
 
-const Element = ({ time, trackStyle, style, id, title, start, end }) =>
-  <div
-    key={id}
-    className="track__element"
-    style={time.toStyleLeftAndWidth(start, end)}
-  >
-    <BasicElement
-      title={title}
-      style={{ ...trackStyle, ...style }}
-    />
-  </div>
+const Element = ({ time, trackStyle, style, id, title, start, end }) => {
+  const width = parseInt(time.toStyleLeftAndWidth(start, end).width, 10)
+  const paddingStyle = width < 20 && { padding: `0 0 0 ${width}px` }
+  return (
+    <div
+      key={id}
+      className="track__element"
+      style={time.toStyleLeftAndWidth(start, end)}
+    >
+      <BasicElement
+        title={title}
+        style={{ ...trackStyle, ...style, ...paddingStyle }}
+      />
+    </div>
+  )
+}
 
 Element.propTypes = {
   time: PropTypes.shape({}).isRequired,
