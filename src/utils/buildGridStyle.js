56d9db0cee633802cb6b41201f52e69d9361f72b
Robert Smith <rbrtsmith84@googlemail.com>
2017-06-08 16:25:38 +0100

    Remove redundant constant.

--- src/utils/buildGridStyle.js
+++ src/utils/buildGridStyle.js
@@ -1,8 +1,7 @@
 const getStartXs = (time, { cells }) => cells.map(cell => time.toX(cell.start))
 
 const buildGridStyle = (time, timebar) => {
-  const StartXs = getStartXs(time, timebar)
-  const gradient = StartXs.reduce((str, x) =>
+  const gradient = getStartXs(time, timebar).reduce((str, x) =>
     `${str} white ${x}px, #eee ${x}px, #eee ${x + 1}px, white ${x + 1}px,`
   , '').slice(0, -1)
   return { background: `linear-gradient(to right, ${gradient})` }
